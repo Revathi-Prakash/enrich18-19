cf61d10f7c1a7e87415d77a88dc6f7f6f70cff2c
Revathi-Prakash <33799713+Revathi-Prakash@users.noreply.github.com>
2018-03-14 08:58:42 -0700

    Update DetOfMatrix.c

--- DetOfMatrix.c
+++ DetOfMatrix.c
@@ -1,24 +1,24 @@
-
 #include <stdio.h>
 float DetOfMat(int flag,int n,float a[n][n])
 {
-    int i,j,k,m;
-	float c,det=1;
+     int i,j,k;
+     float c,det=1;
 	   
-	for(i=0; i<n; i++)
-    {
-	    for(j=0; j<i; j++)
-	    {
+     for(i=0; i<n; i++)
+     {
+	 for(j=0; j<i; j++)
+	 {
             c= a[i][j]/a[j][j];
+		 
             for(k=0;k<n;k++)
             {
                 a[i][k]=a[i][k]-(c*a[j][k]);
-	        }
 	    }
-	}
-    /* when flag=1,it indicates that interchange of rows.
-    When two rows of a matrix are interchanged then the  determinant of
-    the resultant matrix is equal to (-1*determinant) of the matrix.*/
+	 }
+      }
+    	/* when flag=1,it indicates that interchange of rows.
+    	When two rows of a matrix are interchanged then the  determinant of
+    	the resultant matrix is equal to (-1*determinant) of the matrix.*/
 
     for(i=0; i<n; i++)
     {
@@ -43,14 +43,16 @@ int main()
 	scanf("%d",&n);
 	float a[n][n],det;
 	printf("Enter the elements of the matrix");
+	
 	for(i=0; i<n; i++)
-    {
+    	{
 	    for(j=0; j<n; j++)
 	    {
 	        scanf("%f",&a[i][j]);
 	    }   
         
-    }
+    	}
+    
 	j=0;
 	if(a[0][0]==0)
 	{
@@ -70,9 +72,9 @@ int main()
 	        a[0][j]=temp;
 	    }
     
-    }
-    det=DetOfMat(flag,n,a);
-    printf("%0.f",det);
-	    return 0;
-	}
+    	}
+    	det=DetOfMat(flag,n,a);
+    	printf("%0.f",det);
+	return 0;
+}
 
