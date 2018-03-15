9829bf8e6ed7f259b12d9432348ec1b6482a66ef
Revathi-Prakash <33799713+Revathi-Prakash@users.noreply.github.com>
2018-03-15 08:53:55 -0700

    Update MagicSquare.c

--- MagicSquare.c
+++ MagicSquare.c
@@ -1,57 +1,57 @@
-#include <stdio.h>
+#include<stdio.h>
 int magicSquare(int n)
 {
-   int i,j,c,sqr;
-   int a[10][10]={0};
-   sqr=n*n;
-   i=0;
-   j=n>>1;
-   //printf("%d",j);
-   for(c=1;c<=sqr;c++)
-   {
-       
-      if(a[i][j]==0)
-      {
-          a[i][j]=c;
-          i=i-1;
-          j=j+1;
-      }
-      else
-      {
-          while(a[i][j]!=0)
-          {
-              i++;
-          }
-          a[i][j]=c;
-      }
-      if(i<0)
-      {
-          i=n-1;
-      }
-      if(j==n)
-      {
-          j=0; 
-      }
-   }
-  for(i=0;i<n;i++)
-   {
-       for(j=0;j<n;j++)
-       {
-           printf("%d\t",a[i][j]);
-       }
-       printf("\n");
-   }
-}
-int main ()
-{
-  int n;
-  printf ("Enter the n value");
-  scanf("%d",&n);
-  if(n<=0)
-  {
-      printf("value of n should greater than zero");
-      return 0;
-  }
-  magicSquare(n);
-  return 0;
-}
+    int c,a[10][10]={0};
+    int sqr= n*n,i,j,x,y;
+    i= 0;
+    j= n>>1;                      
+    for (c= 1; c<=sqr; c++) 
+    {
+        a[i][j] = c;
+
+        x= i-1;                   // x is decremented to point the row above the current row
+        y= j+1;                  // y is decremented to point the next column
+        if(x<0)
+        {
+            x=n-1;
+        }
+        if(y==n)
+        {
+            y=0;
+        }
+                                  //if the cell already contains a number then move to the cell which is immediate down.
+        if(a[x][y]!=0)  
+        {
+           x=i+1;
+           y=j;
+           if(i+1==n)
+            {
+              i=0;
+            }
+    
+        }
+        i=x;
+        j=y;
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+ }
+ int main()
+ {
+     int n;
+     printf("Enter n value");
+     scanf("%d",&n);
+     if(n<=0)
+     {
+         printf("Value of n should not be negative or zero");
+         return 0;
+     }
+     magicSquare(n);
+     return 0;
+ }
