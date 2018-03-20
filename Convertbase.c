9d57bfaa4caad39da5d74b646e65c0162a30dca9
Revathi-Prakash <33799713+Revathi-Prakash@users.noreply.github.com>
2018-03-20 08:52:59 -0700

    Create Convertbase.c

---
+++ Convertbase.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+//representation of the decimal number with the given base 
+int convertbase(int num,int base)
+{
+    int rem[10],temp=0,i=0,j;
+    while(num>0)
+    {
+        rem[i]=num%base;                    
+        num=num/base;
+        i++;
+    }
+    i--;
+    while(i>=0)
+    {
+        temp=rem[i]+temp*10;         
+        i--;
+    }
+    printf("%d",temp);
+    return 0;
+}
+
+int main()
+{
+    int num,base;
+    printf("Enter the decimal number");
+    scanf("%d",&num);
+    printf("Enter the base");
+    scanf("%d",&base);
+    if(base<0)
+    {
+        printf("base of the number should be positive");
+        return 0;
+    }
+    if(num==0)
+    {
+        printf("%d",num);
+        return 0;
+    }
+    convertbase(num,base);
+}
