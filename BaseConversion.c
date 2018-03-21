784e9b02671129d8e1353c8e4f7af8b04faa6f7f
Revathi-Prakash <33799713+Revathi-Prakash@users.noreply.github.com>
2018-03-21 07:24:54 -0700

    Create BaseConversion.c

---
+++ BaseConversion.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/*
+The number with the given base is equal to the remainder 
+obtained from dividing the given decimal num by the base.
+*/
+int convertbase(int num,int base)
+{
+    int rem,temp=0,i=1;
+    while(num>0)
+    {
+        rem=num%base;  
+        temp=temp+(i*rem);       //remainder at each iteration is multiplied with multiples of ten.
+        num=num/base;
+        i=i*10;      
+    }
+    
+    printf("%d",temp);
+    return 0;
+}
+
+int main()
+{
+    int num,base;
+    printf("Enter the decimal number");
+    scanf("%d",&num);
+    printf("Enter the base");
+    scanf("%d",&base);
+    if(base<0)
+    {
+        printf("base of the number should be positive");
+        return 0;
+    }
+    if(num==0)
+    {
+        printf("%d",num);
+        return 0;
+    }
+    convertbase(num,base);
+    return 0;
+}
